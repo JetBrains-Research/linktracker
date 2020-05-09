cb6255a2ed4a5c3b42d040c1362d1ee2a46b665c
Tudor <t.popovici@student.tudelft.nl>
2020-05-09 16:41:29 +0300

    Add potential link list as the return of getLinks(). Move link matching and creation to LinkFactory

--- src/main/kotlin/org/intellij/plugin/tracker/services/LinkRetrieverService.kt
+++ src/main/kotlin/org/intellij/plugin/tracker/services/LinkRetrieverService.kt
@@ -2,7 +2,6 @@ package org.intellij.plugin.tracker.services
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
@@ -12,11 +11,8 @@ import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import org.intellij.markdown.flavours.gfm.GFMTokenTypes.GFM_AUTOLINK
-import org.intellij.plugin.tracker.data.Link
-import org.intellij.plugin.tracker.data.LinkType
-import org.intellij.plugin.tracker.data.RelativeLink
-import org.intellij.plugin.tracker.data.WebLink
-import org.intellij.plugin.tracker.data.WebLinkReferenceType
+import org.intellij.plugin.tracker.data.links.Link
+import org.intellij.plugin.tracker.data.links.PotentialLink
 import org.intellij.plugins.markdown.lang.MarkdownElementType
 import org.intellij.plugins.markdown.lang.MarkdownElementTypes.AUTOLINK
 import org.intellij.plugins.markdown.lang.MarkdownElementTypes.LINK_DESTINATION
@@ -30,12 +26,12 @@ class LinkRetrieverService(private val project: Project?) {
     var noOfFiles = 0
     var noOfFilesWithLinks = 0
     var linkFound = false
-    var links: ArrayList<Link> = arrayListOf<Link>()
+    var potentialLinkList: ArrayList<PotentialLink> = arrayListOf<PotentialLink>()
 
     /**
      * Function to get the list of links.
      */
-    fun getLinks(): List<Link> {
+    fun getLinks(): MutableList<PotentialLink> {
         val currentProject = project
         val virtualFiles =
             FileTypeIndex.getFiles(MarkdownFileType.INSTANCE, GlobalSearchScope.projectScope(currentProject!!))
@@ -43,6 +39,7 @@ class LinkRetrieverService(private val project: Project?) {
         noOfLinks = 0
         noOfFiles = 0
         noOfFilesWithLinks = 0
+
         for (virtualFile in virtualFiles) {
             linkFound = false
             noOfFiles++
@@ -52,17 +49,34 @@ class LinkRetrieverService(private val project: Project?) {
             psiFile.accept(object : PsiRecursiveElementVisitor() {
                 override fun visitElement(element: PsiElement) {
                     val elemType = element.node.elementType
-                    if (element.javaClass == LeafPsiElement::class.java && (elemType === MarkdownElementType.platformType(GFM_AUTOLINK) &&
-                                element.parent.node.elementType !== LINK_DESTINATION)) {
-                        val linkText = element.node.text
-                        addLink(element, document, linkText, linkText, fileName)
+
+                    val linkText: String
+                    val linkPath: String
+                    val textOffset: Int
+                    val lineNumber: Int
+
+                    if (element.javaClass == LeafPsiElement::class.java && (elemType === MarkdownElementType.platformType(
+                            GFM_AUTOLINK
+                        ) &&
+                                element.parent.node.elementType !== LINK_DESTINATION)
+                    ) {
+                        linkText = element.node.text
+                        textOffset = element.node.startOffset
+                        lineNumber = document.getLineNumber(textOffset) + 1
+                        potentialLinkList.add(PotentialLink(linkText, linkText, fileName, lineNumber))
+
                     } else if (element.javaClass == MarkdownLinkDestinationImpl::class.java && elemType === LINK_DESTINATION) {
-                        val linkPath = element.node.text
-                        val linkText = element.parent.firstChild.node.text.replace("[", "").replace("]", "")
-                        addLink(element, document, linkText, linkPath, fileName)
+                        linkText = element.parent.firstChild.node.text.replace("[", "").replace("]", "")
+                        linkPath = element.node.text
+                        textOffset = element.node.startOffset
+                        lineNumber = document.getLineNumber(textOffset) + 1
+                        potentialLinkList.add(PotentialLink(linkText, linkPath, fileName, lineNumber))
+
                     } else if (element.javaClass == ASTWrapperPsiElement::class.java && elemType === AUTOLINK) {
-                        val linkText = element.node.text.replace("<", "").replace(">", "")
-                        addLink(element, document, linkText, linkText, fileName)
+                        linkText = element.node.text.replace("<", "").replace(">", "")
+                        textOffset = element.node.startOffset
+                        lineNumber = document.getLineNumber(textOffset) + 1
+                        potentialLinkList.add(PotentialLink(linkText, linkText, fileName, lineNumber))
                     }
                     super.visitElement(element)
                 }
@@ -71,67 +85,9 @@ class LinkRetrieverService(private val project: Project?) {
                 noOfFilesWithLinks++
             }
         }
-        return links
+        return potentialLinkList
     }
 
-    /**
-     * Function which gives the type of the link.
-     */
-    fun getLinkType(link: String): LinkType {
-        if (link.contains("https") || link.contains("http") || link.contains("www")) {
-            return LinkType.URL
-        } else if (link.contains(".")) {
-            return LinkType.FILE
-        } else {
-            return LinkType.DIRECTORY
-        }
-    }
-
-    /**
-     * Function which creates the link according to its type.
-     */
-    fun createLink(linkText: String, linkPath: String, proveniencePath: String, lineNo: Int): Link {
-        if (getLinkType(linkPath) == LinkType.URL) {
-            if (linkPath.contains("github") || linkPath.contains("gitlab")) {
-                // commit
-                if (linkPath.contains("blob")) {
-                    val parts = linkPath.split("//").get(1).split("/blob/")
-                    val projectName = parts.get(0).split("/").get(parts.get(0).split("/").lastIndex)
-                    val platformName = parts.get(0).split("/").get(0)
-                    val projectOwnerName = parts.get(0).split(platformName + "/").get(1).split("/" + projectName).get(0)
-                    val relativePath = parts.get(1).split(parts.get(1).split("/").get(0) + "/").get(1).split("#L").get(0)
-                    if (parts.get(1).split("#L").get(1).contains("-L")) {
-                        val start = parts.get(1).split("#L").get(1).split("-L").get(0).toInt()
-                        val end = parts.get(1).split("#L").get(1).split("-L").get(1).toInt()
-                        return WebLink(LinkType.LINES, linkText, linkPath, proveniencePath, lineNo, platformName, projectOwnerName, projectName, relativePath, WebLinkReferenceType.COMMIT, linkText, start, start, end)
-                    } else {
-                        val lineReferenced = parts.get(1).split("#L").get(1).toInt()
-                        return WebLink(LinkType.LINE, linkText, linkPath, proveniencePath, lineNo, platformName, projectOwnerName, projectName, relativePath, WebLinkReferenceType.COMMIT, linkText, lineReferenced, lineReferenced, lineReferenced)
-                    }
-                } else {
-                    val userName = linkPath.split("//").get(1).split("/").get(1)
-                    val platformName = linkPath.split("//").get(1).split("/").get(0)
-                    return WebLink(LinkType.USER, linkText, linkPath, proveniencePath, lineNo, platformName, userName, "", "", WebLinkReferenceType.TAG, linkText, 0, 0, 0)
-                }
-            } else {
-                return WebLink(LinkType.URL, linkText, linkPath, proveniencePath, lineNo, "", "", "", "", WebLinkReferenceType.COMMIT, "", 0, 0, 0)
-            }
-        } else {
-            return RelativeLink(getLinkType(linkPath), linkText, linkPath, proveniencePath, lineNo)
-        }
-    }
-
-    /**
-     * Function to add links to our list.
-     */
-    fun addLink(element: PsiElement, document: Document, linkText: String, linkPath: String, fileName: String) {
-        linkFound = true
-        noOfLinks++
-        val textOffset: Int = element.node.startOffset
-        val lineNumber: Int = document.getLineNumber(textOffset) + 1
-        val link = createLink(linkText, linkPath, fileName, lineNumber)
-        links.add(link)
-    }
 
     /**
      * Used by IDEA to get a reference to the single instance of this class.
