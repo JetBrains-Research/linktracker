1cb59c6b257233d4d5a6ad0536b2d1709fab198f
Tudor <t.popovici@student.tudelft.nl>
2020-05-17 02:40:27 +0300

    Add toString method to LinkChange

--- src/main/kotlin/org/intellij/plugin/tracker/data/changes/LinkChange.kt
+++ src/main/kotlin/org/intellij/plugin/tracker/data/changes/LinkChange.kt
@@ -12,4 +12,8 @@ open class LinkChange(
     open val changeType: ChangeType,
     open val afterPath: String,
     open val errorMessage: String? = null
-)
+) {
+    override fun toString(): String {
+        return "Change type is $changeType and after path is $afterPath"
+    }
+}
