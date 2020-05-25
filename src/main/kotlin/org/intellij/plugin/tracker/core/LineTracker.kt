4e8da1ba80c27612482862dc9aae2214781d088a
Tudor <t.popovici@student.tudelft.nl>
2020-05-25 04:54:22 +0300

    Adapt code to changes retrieved from git diff

--- src/main/kotlin/org/intellij/plugin/tracker/core/LineTracker.kt
+++ src/main/kotlin/org/intellij/plugin/tracker/core/LineTracker.kt
@@ -1,9 +1,12 @@
 package org.intellij.plugin.tracker.core
 
 import com.google.common.collect.HashMultiset
+import org.intellij.plugin.tracker.data.Line
+import org.intellij.plugin.tracker.data.changes.LineChange
+import org.intellij.plugin.tracker.data.links.Link
+import org.intellij.plugin.tracker.data.links.WebLinkToLines
 import org.simmetrics.metrics.CosineSimilarity
 import org.simmetrics.metrics.Levenshtein
-import java.util.*
 import kotlin.collections.ArrayList
 
 
@@ -11,120 +14,215 @@ class LineTracker {
 
     companion object {
 
-        fun trackLine(deletedLine: String, addedLines: List<String>, contextLines: List<List<String>>): Int {
-            val deletedLineContextSimHash: Long = getSimHash(line = getContext(contextLines = contextLines[0]))
-            val deletedLineContentSimHash: Long = getSimHash(line = deletedLine)
-            val possibleList: ArrayList<Triple<String, String, Float>> = arrayListOf()
-            var bestScore: Float = -1f
+        fun trackLines(link: WebLinkToLines, lineChangeList: MutableList<LineChange>): Line? =
+            throw NotImplementedError("")
 
-            for ((index: Int, line: String) in addedLines.withIndex()) {
-                val simHashContextAddedLine: Long = getSimHash(line = getContext(contextLines[index + 1]))
-                val simHashContentAddedLine: Long = getSimHash(line = line)
-                val scoreOfContext: Int = hamming(deletedLineContextSimHash, simHashContextAddedLine)
-                val scoreOfContent: Int = hamming(deletedLineContentSimHash, simHashContentAddedLine)
-                val overallScore: Float = 0.40f * scoreOfContext + 0.60f * scoreOfContent
+        fun trackLine(link: Link, originalLineContent: String, lineChangeList: MutableList<LineChange>): Line? {
+            var lineToTrack: Int = link.getLineReferenced()
+            var addedLines: MutableList<Line> = mutableListOf()
+            var modifications = 0
 
-                if (overallScore >= bestScore) {
-                    bestScore = overallScore
-                    possibleList.add(Triple(deletedLine, line, overallScore))
+            for (change: LineChange in lineChangeList) {
+                // get the deleted line between the commits
+                val deletedLines: MutableList<Line> = change.deletedLines
+                // get the added lines between the commits
+                addedLines = change.addedLines
+
+                // try to find from the deleted lines list a line which has the same line number
+                // which we are looking for
+                val deletedLine: Line? = deletedLines.find { line -> line.lineNumber == lineToTrack }
+
+                // we found the line
+                if (deletedLine != null) {
+                    // join its context lines together in a string
+                    val joinedStringContextLines: String = getJoinedStringContextLines(line=deletedLine)
+
+                    // calculate the SimHash value for both the line contents and concatenated context lines
+                    val deletedLineContextSimHash: Long = getSimHash(line = joinedStringContextLines)
+                    val deletedLineContentSimHash: Long = getSimHash(line = deletedLine.content)
+                    // list of the added lines together with the calculated overall
+                    // hamming score
+                    val possibleList: ArrayList<Pair<Line, Float>> = arrayListOf()
+
+                    for (line: Line in addedLines) {
+                        val joinedStringContextLines1: String = getJoinedStringContextLines(line=line)
+
+                        // calculate the SimHash values of added lines joined context lines
+                        // and added line contents respectively
+                        val simHashContextAddedLine: Long = getSimHash(line = joinedStringContextLines1)
+                        val simHashContentAddedLine: Long = getSimHash(line = line.content)
+
+                        // calculate the hamming distance between the previously SimHash values
+                        val scoreOfContext: Int = hamming(deletedLineContextSimHash, simHashContextAddedLine)
+                        val scoreOfContent: Int = hamming(deletedLineContentSimHash, simHashContentAddedLine)
+
+                        val overallScore: Float = 0.40f * scoreOfContext + 0.60f * scoreOfContent
+                        possibleList.add(Pair(line, overallScore))
+                    }
+
+                    if (possibleList.size > 0) {
+                        // sort by the overall score in ascending order
+                        // lower scores mean that the two strings are more similar
+                        // e.g. a small hamming distance between 2 strings
+                        // will show that the 2 strings are similar
+                        possibleList.sortBy { pair -> pair.second }
+                        val result: Line? = mapLine(deletedLine, possibleList)
+                        if (result != null) {
+                            modifications++
+                            // found a match in possibleList! Update `lineToTrack` to the matching
+                            // line's line number.
+                            lineToTrack = result.lineNumber
+                        }
+                    } else {
+                        // see how many lines were deleted before the line to track
+                        val deletedLinesBefore: Int = deletedLines.count { line -> line.lineNumber < lineToTrack }
+                        // see how many lines were added before the line to track
+                        // these lines that are deleted/added before would influence the current location of `lineToTrack`
+                        var addedLinesBefore: Int = addedLines.count { line -> line.lineNumber < lineToTrack }
+                        // check whether there is an added line which has the same line number as `lineToTrack`
+                        val find: Int = addedLines.indexOfFirst { line -> line.lineNumber == lineToTrack }
+
+                        // if so, get the number of consecutive lines starting at `lineToTrack`
+                        // and add this number to the total of `addedLinesBefore`
+                        if (find != -1) {
+                            addedLinesBefore++
+                            for (index: Int in find + 1 until addedLines.size) {
+                                if (addedLines[index - 1].lineNumber + 1 == addedLines[index].lineNumber)
+                                    addedLinesBefore++
+                            }
+                        }
+
+                        // update the new location of the line according to the deleted/added lines
+                        // before it
+                        lineToTrack += (addedLinesBefore - deletedLinesBefore)
+                    }
+
+
+                    // try to see whether the line was split.
+                    val lineSplit: Pair<Line?, Int> = detectLineSplit(deletedLine.content, addedLines)
+
+                    // for now, use the split line number for further calculations instead
+                    // TODO: track the group of lines further
+                    if (lineSplit.first != null) {
+                        modifications++
+                        lineToTrack = lineSplit.first!!.lineNumber
+                    }
                 }
             }
 
-            // TODO: find k closest neighbours.
-            if (possibleList.size > 0) {
-                val result: Int = mapLine(deletedLine, contextLines, possibleList)
-                if (result != -1) return result
-            } else {
-                return -1
-            }
-
-            // no match until now.
-            // try to see whether the line was split.
-            return detectLineSplit(deletedLine, addedLines, contextLines, possibleList)
+            // if no modifications have been made to the contents of the line, return
+            // the original content
+            if (modifications == 0) return Line(lineToTrack, originalLineContent)
+            // else, returns the line from added lines list which corresponds to
+            // `lineToTrack`
+            return addedLines.find { line -> line.lineNumber == lineToTrack }
         }
 
-        private fun detectLineSplit(
-            deletedLine: String,
-            addedLines: List<String>,
-            contextLines: List<List<String>>,
-            possibleList: List<Triple<String, String, Float>>
-        ): Int {
-            val thresholdValue = 0.85f
-            val maxConcatenationLines = 8
+        private fun getJoinedStringContextLines(line: Line): String {
+            return if (line.contextLines == null) ""
+            else line.contextLines!!.joinToString()
+        }
+
+        private fun detectLineSplit(deletedLine: String, addedLines: List<Line>, thresholdValue: Float = 0.85f): Pair<Line?, Int> {
+            // initialize helper variables
             var bestScore = -1f
             var bestIndex = -1
+            var concatenatedLines = 0
+            var bestConcatenatedLines: Int = concatenatedLines
 
-            for ((index: Int, line: String) in addedLines.withIndex()) {
-                val previousScore: Float = Levenshtein().compare(deletedLine, line)
-                var concatenateString: String = line
+            // go over each added line
+            for (i: Int in 0 until addedLines.size - 1) {
+                concatenatedLines = 1
+                // calculate the levenshtein distance between the deleted line and this added line
+                var previousScore: Float = Levenshtein().compare(deletedLine, addedLines[i].content)
+                var concatenateString: String = addedLines[i].content
 
-                for (i: Int in 0 until contextLines[index + 1].size) {
-                    concatenateString += contextLines[index + 1][i]
-                    val currentScore: Float = Levenshtein().compare(
-                        deletedLine, concatenateString
-                    )
+                for (j: Int in i + 1 until addedLines.size) {
+                    // concatenate the next added line
+                    concatenateString += addedLines[j].content
 
-                    if (currentScore >= bestScore && currentScore >= thresholdValue) {
+                    // increment the # of concatenated lines
+                    concatenatedLines++
+
+                    // compare the current score of the deleted line (which is the original line) contents
+                    // with the concatenated string
+                    val currentScore: Float = Levenshtein().compare(deletedLine, concatenateString)
+
+                    // if the score gets getting better with each concatenation
+                    // save it. It might mean that the line is being built-up to its original format
+                    // with each concatenation that we are doing.
+                    if (currentScore >= bestScore) {
                         bestScore = currentScore
-                        bestIndex = index
+                        bestIndex = i
+                        bestConcatenatedLines = concatenatedLines
                     }
 
-                    if (currentScore <= previousScore) {
-                        break
-                    }
+                    // if a concatenation would cause the score to go below the previous score
+                    // just break. It means that going further with the loop won't make a difference
+                    if (currentScore <= previousScore) break
+                    previousScore = currentScore
                 }
             }
-            return bestIndex
+
+            // if the best score goes over a pre-defined threshold, return the index in addedLines
+            // at which the line split begins, accompanied with the number of lines over which the line
+            // is split
+            if (bestScore >= thresholdValue) return Pair(addedLines[bestIndex], bestConcatenatedLines)
+            // no line split found, return null and 0 respectively
+            return Pair(null, 0)
         }
 
-        private fun mapLine(
-            deletedLine: String,
-            contextLines: List<List<String>>,
-            possibleList: List<Triple<String, String, Float>>
-        ): Int {
+        private fun mapLine(deletedLine: Line, possibleList: List<Pair<Line, Float>>, thresholdValue: Float = 0.7f): Line? {
+            // initialize helper variables
             var mappingFound = false
             var bestScore = 0.45F
             var bestIndex = -1
 
-            Collections.sort(possibleList, object : Comparator<Triple<String, String, Float>> {
-                override fun compare(o1: Triple<String, String, Float>?, o2: Triple<String, String, Float>?): Int {
-                    if (o2!!.third > o1!!.third) return -1
-                    else if (o2.third < o1.third) return 1
-                    return 0
-                }
-            })
-
+            // go over each pair in possibleList
+            // this pair contains the added line accompanied by the hamming distance overall score
+            // between the deleted line and this added line
+            // possibleList is ordered according to overall score in INCREASING order!
             for (i: Int in 0 until possibleList.size) {
-                val scoreContent: Float = Levenshtein().compare(deletedLine, possibleList[i].second)
+                // calculate the levenshtein distance between the deleted line and currently inspected added line
+                val scoreContent: Float = Levenshtein().compare(deletedLine.content, possibleList[i].first.content)
 
+                // initialize a hash multiset containing the context lines for the deleted line
+                // and a hash multiset for the context lines of the currently inspected added line
                 val multiSet1: HashMultiset<String> = HashMultiset.create()
-                for (el: String in contextLines[0]) multiSet1.add(el)
+                if (deletedLine.contextLines != null)
+                    for (el: Line in deletedLine.contextLines!!) multiSet1.add(el.content)
                 val multiSet2: HashMultiset<String> = HashMultiset.create()
-                for (el: String in contextLines[i + 1]) multiSet2.add(el)
+                if (possibleList[i].first.contextLines != null)
+                    for (el: Line in possibleList[i].first.contextLines!!.iterator()) multiSet2.add(el.content)
 
+                // calculate the cosine similarity between the context lines of the deleted line
+                // and the context lines of the currently inspected added line
                 val scoreContext: Float = CosineSimilarity<String>().compare(multiSet1, multiSet2)
                 val score: Double = 0.6 * scoreContent + 0.4 * scoreContext
+
+                // if the overall score is best so far, save the details
                 if (score >= bestScore) {
                     mappingFound = true
                     bestIndex = i
                     bestScore = score.toFloat()
                 }
             }
+
+            // if we found a mapping so far and this mapping is bigger than the threshold value
+            // return the matching line
+            // otherwise, return null
             return if (mappingFound) {
-                bestIndex
+                if (bestScore >= thresholdValue) return possibleList[bestIndex].first
+                return null
             } else {
-                -1
+                null
             }
         }
 
-        private fun getContext(contextLines: List<String>): String {
-            val stringBuilder = StringBuilder()
-            for (j: Int in 0 until contextLines.size)
-                if (!contextLines[j].matches(Regex("\\s+")) && !contextLines[j].trim().matches(Regex("\\{ | \\( | \\)| \\}")))
-                    stringBuilder.append(contextLines[j])
-            return stringBuilder.toString()
-        }
-
+        /**
+         * Does a bit-wise hamming distance between two long values.
+         * Returns the hamming distance
+         */
         private fun hamming(first: Long, second: Long): Int {
             var x: Long = first xor second
             var setBits = 0L
@@ -136,14 +234,25 @@ class LineTracker {
             return setBits.toInt()
         }
 
+        /**
+         * Method that calculates the sim hash value of a given string (line)
+         *
+         * Breaks the string into shingles (list of fragments of the string)
+         * and hashes each shingle using a hashing algorithm (in this case Jenkins).
+         *
+         * If the i-th bit of the hashed shingle is set, add a 1 to the bit-array
+         * in position i, else subtract 1 from position i of that bit-array
+         *
+         * Sim hash bit i will be 1 if the bit array at position i is > 0, 0 otherwise
+         */
         private fun getSimHash(line: String, hashSize: Int = 32): Long {
             val bitArray = IntArray(hashSize) { 0 }
             val shingleList: ArrayList<String> = createShingles(line)
             var simHashValue: Long = 0
 
-            for (shingle in shingleList) {
-                val hashedShingle = JenkinsHash().hash(shingle.toByteArray())
-                for (i in 0 until hashSize) {
+            for (shingle: String in shingleList) {
+                val hashedShingle: Long = JenkinsHash().hash(shingle.toByteArray())
+                for (i: Int in 0 until hashSize) {
                     bitArray[i] += if (ithBitSet(hashedShingle, i)) 1 else -1
                 }
             }
@@ -158,19 +267,35 @@ class LineTracker {
             return simHashValue
         }
 
+        /**
+         * Verifies whether the i-th bit of a long number in binary format is set
+         */
         private fun ithBitSet(hashedShingle: Long, position: Int): Boolean {
             if (hashedShingle and (1L shl position) == 1L)
                 return true
             return false
         }
 
+        /**
+         * Creates shingles (a list of fragments) out of a given string (line)
+         * using a specified size for the shingles.
+         *
+         * Shingles are created by taking substrings of `shingle size` each
+         * out of the initial string and appending them to a list
+         */
         private fun createShingles(line: String, shingleSize: Int = 2): ArrayList<String> {
             val tokenList = ArrayList<String>()
-            var replacedLine: String = line.replace(Regex("\\r\\n|\\n|\\r"), " ")
-            replacedLine = replacedLine.replace(Regex("\\s+"), "")
-            val stringBuilder: StringBuilder = StringBuilder().append(replacedLine)
-            if (line.length % shingleSize != 0)
-                replacedLine = stringBuilder.append("_".repeat(line.length % shingleSize)).toString()
+
+            // remove all white spaces from the line
+            var replacedLine = line.replace(Regex("\\s+"), "")
+
+            if (line.length % shingleSize != 0) {
+                var difference: Int = line.length % shingleSize
+                while (difference != 0) {
+                    replacedLine += "~"
+                    difference--
+                }
+            }
 
             var i = 0
             while (i + shingleSize < replacedLine.length) {
