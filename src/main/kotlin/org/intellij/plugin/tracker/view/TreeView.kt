d70a16177bd12ce5edc9c9575150c3a7a283e3e8
Ceren Ugurlu <c.ugurlu@student.tudelft.nl>
2020-05-26 02:11:12 +0200

    Functionality to direct user to link's location

--- src/main/kotlin/org/intellij/plugin/tracker/view/TreeView.kt
+++ src/main/kotlin/org/intellij/plugin/tracker/view/TreeView.kt
@@ -1,14 +1,20 @@
 package org.intellij.plugin.tracker.view
 
+import com.intellij.openapi.actionSystem.ActionToolbar
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.ex.ActionManagerEx
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.vfs.VfsUtil
 import org.intellij.plugin.tracker.data.changes.ChangeType
 import org.intellij.plugin.tracker.data.changes.LinkChange
 import org.intellij.plugin.tracker.data.links.Link
+import org.jdesktop.swingx.action.ActionManager
 import java.awt.BorderLayout
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import java.awt.event.MouseListener
+import java.io.File
 import javax.swing.*
-import javax.swing.event.TreeSelectionListener
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeModel
 
@@ -38,13 +44,20 @@ class TreeView : JPanel(BorderLayout()) {
         val unchanged = DefaultMutableTreeNode("Unchanged Links ${unchangedOnes.size} links")
         val invalid = DefaultMutableTreeNode("Invalid Links ${invalidOnes.size} links")
 
+        val info = changes.map {
+            mutableListOf(it.first.linkInfo.linkPath, it.first.linkInfo.proveniencePath,
+                    it.first.linkInfo.foundAtLineNumber) }
+
+        callListener(info)
+
         root.add(addNodeTree(changedOnes, changed))
         root.add(addNodeTree(unchangedOnes, unchanged))
         root.add(addNodeTree(invalidOnes, invalid))
         (tree.model as DefaultTreeModel).reload()
     }
 
-    private fun addNodeTree(changeList: Map<String, List<Pair<Link, LinkChange>>>, tree: DefaultMutableTreeNode): DefaultMutableTreeNode {
+    private fun addNodeTree(changeList: Map<String, List<Pair<Link, LinkChange>>>, node: DefaultMutableTreeNode):
+            DefaultMutableTreeNode {
         for (linkList in changeList) {
             val fileName = linkList.value[0].first.linkInfo.fileName
             var path = linkList.key.replace(fileName, "")
@@ -53,13 +66,49 @@ class TreeView : JPanel(BorderLayout()) {
             }
             val file = DefaultMutableTreeNode("$fileName $path")
             for (links in linkList.value) {
-                val link  = DefaultMutableTreeNode(links.first.linkInfo.linkPath)
-                link.add(DefaultMutableTreeNode("(${links.first.linkInfo.foundAtLineNumber}, ${links.first.linkInfo.textOffset}) ${links.first.linkInfo.linkText}"))
+                val link = DefaultMutableTreeNode(links.first.linkInfo.linkPath)
+                link.add(DefaultMutableTreeNode("(${links.first.linkInfo.foundAtLineNumber}) " +
+                        links.first.linkInfo.linkText))
                 file.add(link)
             }
-            tree.add(file)
+            node.add(file)
         }
-        return tree
+
+        return node
+    }
+
+    private fun callListener(info: List<MutableList<*>>) {
+        val treePopup = TreePopup(tree)
+        tree.addMouseListener(object : MouseAdapter() {
+            override fun mouseReleased(e: MouseEvent) {
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    val selRow = tree.getRowForLocation(e.x, e.y)
+                    val selPath = tree.getPathForLocation(e.x, e.y)
+                    if (selPath.pathCount == 4) {
+                        tree.selectionPath = selPath
+                        if (selRow > -1) {
+                            tree.setSelectionRow(selRow)
+                        }
+                        treePopup.show(e.component, e.x, e.y)
+                    }
+                }
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    val selPath = tree.getPathForLocation(e.x, e.y)
+                    if (selPath.pathCount == 4) {
+                        val project = ProjectManager.getInstance().openProjects[0]
+                        val name = selPath.lastPathComponent.toString()
+                        for (information in info) {
+                            if (information[0].toString() == name) {
+                                val file = File(project.basePath + "/" + information[1])
+                                val virtualFile = VfsUtil.findFileByIoFile(file, true)
+                                OpenFileDescriptor(project, virtualFile!!, information[2] as Int - 1,
+                                        0).navigate(true)
+                            }
+                        }
+                    }
+                }
+            }
+        })
     }
 
     /**
@@ -75,31 +124,12 @@ class TreeView : JPanel(BorderLayout()) {
         (tree.model as DefaultTreeModel).reload()
         tree.isRootVisible = false
         tree.cellRenderer = CustomCellRenderer()
-
-        // right click listener
-        val treePopup = TreePopup(tree)
-        tree.addMouseListener(object : MouseAdapter() {
-            override fun mouseReleased(e: MouseEvent) {
-                if (SwingUtilities.isRightMouseButton(e)) {
-                    val selRow = tree.getRowForLocation(e.x, e.y)
-                    val selPath = tree.getPathForLocation(e.x, e.y)
-                    if (selPath.pathCount == 4) {
-                        tree.selectionPath = selPath
-                        if (selRow > -1) {
-                            tree.setSelectionRow(selRow)
-                        }
-                        treePopup.show(e.component, e.x, e.y)
-                    }
-                }
-            }
-        })
-
         val scrollPane = JScrollPane(tree)
         add(scrollPane, BorderLayout.CENTER)
     }
 }
 
-internal class TreePopup(tree: JTree?) : JPopupMenu() {
+class TreePopup(tree: JTree?) : JPopupMenu() {
     init {
         val add = JMenuItem("Accept Change")
         val delete = JMenuItem("Deny Change")
