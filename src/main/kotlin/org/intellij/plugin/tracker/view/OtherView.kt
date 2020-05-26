f86c82eb50d611e27f46f2ccbf9ca99a08ac616d
Ceren Ugurlu <c.ugurlu@student.tudelft.nl>
2020-05-26 17:57:25 +0200

    Add check box tree

--- src/main/kotlin/org/intellij/plugin/tracker/view/OtherView.kt
+++ src/main/kotlin/org/intellij/plugin/tracker/view/OtherView.kt
@@ -2,11 +2,34 @@ package org.intellij.plugin.tracker.view
 
 import java.awt.BorderLayout
 import javax.swing.JPanel
+import javax.swing.WindowConstants
+import javax.swing.tree.TreePath
 
 /**
  * Class creating other view
  */
 class OtherView : JPanel(BorderLayout()) {
+
+    init {
+        val contentPane = JPanel()
+        val cbt = JCheckBoxTree()
+        cbt.addCheckChangeEventListener(object : JCheckBoxTree.CheckChangeEventListener {
+            override fun checkStateChanged(event: JCheckBoxTree.CheckChangeEvent?) {
+                println("event")
+                val paths: Array<TreePath> = cbt.getCheckedPaths()
+                for (tp in paths) {
+                    for (pathPart in tp.getPath()) {
+                        print("$pathPart,")
+                    }
+                    println()
+                }
+            }
+        })
+        contentPane.layout = BorderLayout()
+        contentPane.add(cbt)
+        add(contentPane, BorderLayout.CENTER)
+    }
+
 
 }
 
