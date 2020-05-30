a6f2bb1cf4ee0a04f9d00989bd40062538d6ead2
Tudor <t.popovici@student.tudelft.nl>
2020-05-30 03:27:44 +0300

    Add ChangeTrackerService interface

---
+++ src/main/kotlin/org/intellij/plugin/tracker/services/ChangeTrackerService.kt
@@ -0,0 +1,35 @@
+package org.intellij.plugin.tracker.services
+
+import org.intellij.plugin.tracker.data.changes.Change
+import org.intellij.plugin.tracker.data.links.Link
+
+interface ChangeTrackerService {
+
+    fun getLocalFileChanges(
+        link: Link,
+        branchOrTagName: String? = null,
+        specificCommit: String? = null
+    ): Change
+
+    fun getLocalDirectoryChanges(link: Link): Change
+
+    fun getLocalLineChanges(
+        link: Link,
+        branchOrTagName: String? = null,
+        specificCommit: String? = null
+    ): Change
+
+    fun getLocalLinesChanges(
+        link: Link,
+        branchOrTagName: String? = null,
+        specificCommit: String? = null
+    ): Change
+
+    fun getRemoteFileChanges(link: Link): Change
+
+    fun getRemoteDirectoryChanges(link: Link): Change
+
+    fun getRemoteLineChanges(link: Link): Change
+
+    fun getRemoteLinesChanges(link: Link): Change
+}
