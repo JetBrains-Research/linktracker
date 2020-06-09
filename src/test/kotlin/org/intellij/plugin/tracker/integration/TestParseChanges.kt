f22713899408e65a3c290c9a0577e8febd340552
Tommaso Brandirali <t.brandirali@student.tudelft.nl>
2020-06-09 20:24:41 +0200

    Cloned sources from git4idea tests

--- src/test/kotlin/org/intellij/plugin/tracker/integration/TestParseChanges.kt
+++ src/test/kotlin/org/intellij/plugin/tracker/integration/TestParseChanges.kt
@@ -1,17 +1,7 @@
 package org.intellij.plugin.tracker.integration
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.vcs.AbstractVcsHelper
-import com.intellij.openapi.vcs.VcsTestUtil
-import com.intellij.openapi.vcs.changes.CommitContext
-import com.intellij.testFramework.replaceService
-import com.intellij.vcs.test.VcsPlatformTest
-import git4idea.GitUtil
-import git4idea.GitVcs
-import git4idea.commands.Git
-import git4idea.commands.GitCommand
-import git4idea.commands.GitLineHandler
-import git4idea.repo.GitRepositoryManager
+import org.intellij.plugin.tracker.integration.git4idea.test.GitSingleRepoTest
+import org.intellij.plugin.tracker.utils.GitOperationManager
 import org.junit.jupiter.api.*
 
 /**
@@ -20,32 +10,13 @@ import org.junit.jupiter.api.*
  * it is necessary to create a different instance of this class for each test case.
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class TestParseChanges : VcsPlatformTest() {
+class TestParseChanges : GitSingleRepoTest() {
 
-    protected lateinit var vcs: GitVcs
-    protected lateinit var commitContext: CommitContext
-    protected lateinit var repositoryManager: GitRepositoryManager
-    protected lateinit var vcsHelper: MockVcsHelper
-    protected lateinit var git: TestGitImpl
-
-    private val commitCommand = GitCommand.COMMIT
-    private val addCommad = GitCommand.ADD
-    private lateinit var addAllHandler: GitLineHandler
-    private lateinit var commitHandler: GitLineHandler
+    private lateinit var gitOperationManager: GitOperationManager
 
     @BeforeAll
     override fun setUp() {
         super.setUp()
-
-        vcsHelper = MockVcsHelper(myProject)
-        project.replaceService(AbstractVcsHelper::class.java, vcsHelper, testRootDisposable)
-
-        git = TestGitImpl()
-        ApplicationManager.getApplication().replaceService(Git::class.java, git, testRootDisposable)
-        repositoryManager = GitUtil.getRepositoryManager(project)
-        vcs = GitVcs.getInstance(project)
-        vcs.doActivate()
-        commitContext = CommitContext()
     }
 
     @AfterAll
@@ -55,22 +26,21 @@ class TestParseChanges : VcsPlatformTest() {
 
     @BeforeEach
     fun config() {
-        git.init(project, testRootFile)
-        addAllHandler = GitLineHandler(project, testRootFile, addCommad)
-        addAllHandler.addParameters(".")
-        commitHandler = GitLineHandler(project, testRootFile, commitCommand)
+        gitOperationManager = GitOperationManager(project)
     }
 
-    @Disabled
     @Test
     fun test() {
 
-        // Setup
-        val testFile = VcsTestUtil.createFile(project, testRootFile, "TestFile", "Some content")
-        val addResult = git.runCommand(addAllHandler)
-        Assertions.assertTrue(addResult.success())
-        val commitResult = git.runCommand(commitHandler)
-        println(commitResult.errorOutput)
-        Assertions.assertTrue(commitResult.success())
+        val linkedFile = file("file.txt").create("Some content")
+        linkedFile.addCommit("Commit 1")
+
+        val linkingFile = file("file.md").create("[link](file.txt)")
+        linkingFile.addCommit("Commit 2")
+
+        refresh()
+        updateChangeListManager()
+
+        println(gitOperationManager.getHeadCommitSHA())
     }
 }
