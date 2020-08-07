7f94c0e7710b2860c915b5365a57a20355a03a6d
Tudor <t.popovici@student.tudelft.nl>
2020-08-07 03:55:32 +0300

    Add performDiffOutput method for common logic of change sources. Add abstract functions for getting a change for directory

--- src/main/kotlin/org/intellij/plugin/tracker/core/change/ChangeSource.kt
+++ src/main/kotlin/org/intellij/plugin/tracker/core/change/ChangeSource.kt
@@ -5,17 +5,30 @@ import org.intellij.plugin.tracker.data.OriginalLinesContentsNotFoundException
 import org.intellij.plugin.tracker.data.changes.Change
 import org.intellij.plugin.tracker.data.changes.CustomChange
 import org.intellij.plugin.tracker.data.diff.DiffOutput
+import org.intellij.plugin.tracker.data.diff.FileHistory
 import org.intellij.plugin.tracker.data.links.Link
 
 abstract class ChangeSource {
 
     abstract fun getChangeForFile(link: Link): Change
 
-    abstract fun getDiffOutput(fileChange: CustomChange): List<DiffOutput>
+    abstract fun getChangeForDirectory(link: Link): Change
 
     abstract fun getLines(link: Link): List<String>?
 
-    abstract fun getDirectoryInfo(link: Link): DirectoryInfo
+    abstract fun performDiffOutput(before: FileHistory, after: FileHistory): DiffOutput?
+
+    fun getDiffOutput(fileChange: CustomChange): List<DiffOutput> {
+        val diffOutputList: MutableList<DiffOutput> = mutableListOf()
+        val fileHistoryList = fileChange.fileHistoryList
+        if (fileHistoryList.size >= 2) {
+            for (x: Int in 0 until fileHistoryList.size - 1) {
+                val diffOutput = performDiffOutput(fileHistoryList[x], fileHistoryList[x + 1])
+                if (diffOutput != null) diffOutputList.add(diffOutput)
+            }
+        }
+        return diffOutputList
+    }
 
     fun getOriginalLineContents(link: Link): String {
         val lines = getLines(link)
@@ -40,6 +53,6 @@ abstract class ChangeSource {
         throw OriginalLinesContentsNotFoundException()
     }
 
-    data class DirectoryInfo (val movedFiles: List<String>, val directorySize: Int)
+    data class DirectoryInfo(val movedFiles: List<String>, val directorySize: Int)
 
 }
